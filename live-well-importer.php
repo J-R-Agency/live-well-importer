242c273c29b9d5f0a6f85254fd235b007485ab2d
Greg Macoy <greg.macoy@jnragency.co.uk>
2020-02-10 18:10:34 +0000

    Testing custom fields update

--- live-well-importer.php
+++ live-well-importer.php
@@ -122,7 +122,7 @@ function live_well_importer_handle_post(){
 						$acf_posts = get_posts( array('post_title' => 'WebsiteUrl') ) ;
 
 
-						$acf_post = get_page_by_title( "WebsiteUrl", "OBJECT", "acf-field" ) ;
+				//		$acf_post = get_page_by_title( "WebsiteUrl", "OBJECT", "acf-field" ) ;
 
 						$field_key = $acf_post["post_name"];
 						echo " FIELD KEY: " . $field_key ;
