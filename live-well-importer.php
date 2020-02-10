21fb2e11fad765e360903af2daa9c7189a753d01
Greg Macoy <greg.macoy@jnragency.co.uk>
2020-02-10 13:23:04 +0000

    Add skeleton form handler

--- live-well-importer.php
+++ live-well-importer.php
@@ -16,13 +16,38 @@ function live_well_importer_setup_menu(){
 }
  
 function live_well_importer_init(){
+
+	    live_well_importer_handle_post();
+
         echo "<h1>Hello World!</h1>
         <h2>Upload a File</h2>
         <!-- Form to handle the upload - The enctype value here is very important -->
         <form  method=\"post\" enctype=\"multipart/form-data\">
-                <input type=\"file\" id=\"test_upload_pdf\" name=\"test_upload_pdf\"></input>";
-        		submit_button('Upload');
+                <input type=\"file\" id=\"api_xml\" name=\"api_xml\"></input>";
+        		submit_button('Import');
         echo "</form>";
 }
+
+
+function live_well_importer_handle_post(){
+        // First check if the file appears on the _FILES array
+        if(isset($_FILES['api_xml'])){
+                $pdf = $_FILES['api_xml'];
  
+                // Use the wordpress function to upload
+                // test_upload_pdf corresponds to the position in the $_FILES array
+                // 0 means the content is not associated with any other posts
+                $uploaded=media_handle_upload('api_xml', 0);
+                // Error checking using WP functions
+                if(is_wp_error($uploaded)){
+                        echo "Error uploading file: " . $uploaded->get_error_message();
+                }else{
+                        echo "File upload successful!";
+                        //test_convert($uploaded);
+                }
+ 
+ 
+        }
+}
+
 ?>
