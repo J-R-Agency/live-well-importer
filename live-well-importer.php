a5a913ca9935d7bdd49889f6099cbcfa1adb6f2d
Greg Macoy <greg.macoy@jnragency.co.uk>
2020-02-10 16:20:39 +0000

    Skeleton processing structure and initial data import attempt

--- live-well-importer.php
+++ live-well-importer.php
@@ -57,6 +57,66 @@ function live_well_importer_handle_post(){
 					print_r( $data["Services"] );
 				    echo "<pre>";
 
+
+					// First remove all previous imported posts
+					$currentPosts = get_posts(array( 
+						'post_type' 		=> 'activities', // Or "page" or some custom post type
+						'post_status' 		=> 'publish',
+						'meta_key'			=> 'imported', // Our post options to determined
+						'posts_per_page'   	=> 1000 // Just to make sure we've got all our posts, the default is just 5
+					));
+
+					// Loop through them
+					foreach($currentPosts as $post){
+
+						// Get the featured image id
+						if($thumbId = get_post_meta($post->ID,'_thumbnail_id',true)){
+
+							// Remove the featured image
+							wp_delete_attachment($thumbId,true);
+						}
+
+						// Remove the post
+						wp_delete_post( $post->ID, true);
+					}
+
+					// Loop through some items in the xml
+					foreach($data["Services"]->item as $item){
+
+						// Let's start with creating the post itself
+						$postCreated = array(
+							'post_title' 	=> $item->Service,
+							'post_content' 	=> $item->Description,
+							'post_excerpt' 	=> $item->Organisation,
+							'post_status' 	=> 'publish',
+							'post_type' 	=> 'activities', // Or "page" or some custom post type
+						);
+
+						// Get the increment id from the inserted post
+						$postInsertId = wp_insert_post( $postCreated );
+
+						// Our custom post options, for now only some meta's for the
+						// Yoast SEO plugin and a "flag" to determined if a
+						// post was imported or not
+						$postOptions = array(
+							'imported'				=> true
+						);
+
+						// Loop through the post options
+						foreach($postOptions as $key=>$value){
+
+							// Add the post options
+							update_post_meta($postInsertId,$key,$value);
+						}
+
+						// This is a little trick to "catch" the image id
+						// Attach/upload the "sideloaded" image
+						// And remove the little trick
+						// add_action('add_attachment','featuredImageTrick');
+						// media_sideload_image($item->image, $postInsertId, $item->title);
+						// remove_action('add_attachment','featuredImageTrick');
+					}
+
 				} else {
 					echo " Not DATA ";
 				}
