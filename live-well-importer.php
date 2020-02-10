a9b3088c41f3bedf496b1ec239a0a3a90ce1120b
Greg Macoy <greg.macoy@jnragency.co.uk>
2020-02-10 13:11:21 +0000

    Live Well Importer set-up

--- live-well-importer.php
+++ live-well-importer.php
@@ -4,18 +4,18 @@ Plugin Name: Live Well Importer plugin for Wellbeing Liverpool
 Plugin URI: https://www.jnragency.co.uk/
 Description: Live Well Importer plugin for Wellbeing Liverpool
 Version: 0.1
-Author: GM
+Author: Greg Macoy
 Author URI: https://www.jnragency.co.uk/
 */
 
 
-add_action('admin_menu', 'test_plugin_setup_menu');
+add_action('admin_menu', 'live_well_importer_setup_menu');
  
-function test_plugin_setup_menu(){
-        add_menu_page( 'Test Plugin Page', 'Test Plugin', 'manage_options', 'test-plugin', 'test_init' );
+function live_well_importer_setup_menu(){
+        add_menu_page( 'Live Well Importer', 'Live Well Importer', 'manage_options', 'live-well-importer', 'live_well_importer_init' );
 }
  
-function test_init(){
+function live_well_importer_init(){
         echo "<h1>Hello World!</h1>";
 }
  
