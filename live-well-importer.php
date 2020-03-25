c90b23fd8a8be382dff1027dd57c3ec8f1232965
Gregory Macoy <gregorymacoy@Gregorys-MacBook-Pro.local>
2020-03-25 18:30:59 +0000

    Importing key additional API fields

--- live-well-importer.php
+++ live-well-importer.php
@@ -146,6 +146,7 @@ function live_well_importer_handle_post(){
 						// WARNING FIELD NEEDS TO EXIST AND HAVE DATA BEFORE WE CAN ADD TO IT
 						// AND WE NEED TO USE THE FIELD KEY FROM POST META TABLE
 
+						// WebsiteURL
 						$field_key = get_post_meta( $postInsertId, "_" . strtolower("WebsiteUrl"), true );
 						$acf_posts = get_posts( array('post_title' => 'WebsiteUrl') ) ;
 						$acf_post = get_page_by_title( 'WebsiteUrl', OBJECT, 'acf-field' ) ;
@@ -154,7 +155,33 @@ function live_well_importer_handle_post(){
 						// update_field('field_5e418f9203cbd', $item["WebsiteUrl"], $postInsertId);
 						update_field( "$field_key", $item["WebsiteUrl"], $postInsertId);
 
+						// Wellbeing-API-Cost-bracket
+						$field_key = get_post_meta( $postInsertId, "_" . strtolower("Wellbeing-API-Cost-bracket"), true );
+						$acf_posts = get_posts( array('post_title' => 'Wellbeing-API-Cost-bracket') ) ;
+						$acf_post = get_page_by_title( 'Wellbeing-API-Cost-bracket', OBJECT, 'acf-field' ) ;
+						$field_key = $acf_post->post_name;
+						echo " FIELD KEY: " . $field_key ;
+						// update_field('field_5e418f9203cbd', $item["Wellbeing-API-Cost-bracket"], $postInsertId);
+						update_field( "$field_key", $item["Wellbeing-API-Cost-bracket"], $postInsertId);
 
+						// Wellbeing-API-theme
+						$field_key = get_post_meta( $postInsertId, "_" . strtolower("Wellbeing-API-theme"), true );
+						$acf_posts = get_posts( array('post_title' => 'Wellbeing-API-theme') ) ;
+						$acf_post = get_page_by_title( 'Wellbeing-API-theme', OBJECT, 'acf-field' ) ;
+						$field_key = $acf_post->post_name;
+						echo " FIELD KEY: " . $field_key ;
+						// update_field('field_5e418f9203cbd', $item["Wellbeing-API-theme"], $postInsertId);
+						update_field( "$field_key", $item["Wellbeing-API-theme"], $postInsertId);
+
+						// Wellbeing-API-days-of-the-week
+						$field_key = get_post_meta( $postInsertId, "_" . strtolower("Wellbeing-API-days-of-the-week"), true );
+						$acf_posts = get_posts( array('post_title' => 'Wellbeing-API-days-of-the-week') ) ;
+						$acf_post = get_page_by_title( 'Wellbeing-API-days-of-the-week', OBJECT, 'acf-field' ) ;
+						$field_key = $acf_post->post_name;
+						echo " FIELD KEY: " . $field_key ;
+						// update_field('field_5e418f9203cbd', $item["Wellbeing-API-days-of-the-week"], $postInsertId);
+						update_field( "$field_key", $item["Wellbeing-API-days-of-the-week"], $postInsertId);
+												
 						// This is a little trick to "catch" the image id
 						// Attach/upload the "sideloaded" image
 						// And remove the little trick
