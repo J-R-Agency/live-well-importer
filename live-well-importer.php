81df39042a04adf8f1d0d6f0ce7ac42205a69d5b
Gregory Macoy <gregorymacoy@Gregorys-MacBook-Pro.local>
2020-03-25 18:13:07 +0000

    Additional Fields testing Wellbeing-API-theme

--- live-well-importer.php
+++ live-well-importer.php
@@ -95,7 +95,8 @@ function live_well_importer_handle_post(){
 							print_r( $additionalfield["Name"] );
 							echo " AI Field Values: ";
 							print_r( $additionalfield["Values"] );
-
+							echo implode(",", $additionalfield["Values"]);
+							
 							foreach ( $additionalfield["Values"] as $additionalfield_values ){
 								//print_r( $additionalfield_values["string"] );
 								//echo implode(",", $additionalfield_values["string"]);
