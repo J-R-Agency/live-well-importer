d4a979569c1bbb696acc4e594ecb4026f9d5ea85
Greg Macoy <greg.macoy@jnragency.co.uk>
2020-03-27 12:13:34 +0000

    Adding location support

--- live-well-importer.php
+++ live-well-importer.php
@@ -240,6 +240,14 @@ function live_well_importer_handle_post(){
 						// update_field('field_5e418f9203cbd', $item["Wellbeing-API-days-of-the-week"], $postInsertId);
 						update_field( "$field_key", $wellbeing_api_days_of_the_week, $postInsertId);
 
+						// Main Address custom field aggregated for Maps API etc.
+						$field_key = get_post_meta( $postInsertId, "_" . strtolower("Main_Address"), true );
+						$acf_posts = get_posts( array('post_title' => 'Main Address') ) ;
+						$acf_post = get_page_by_title( 'Main Address', OBJECT, 'acf-field' ) ;
+						$field_key = $acf_post->post_name;
+						// echo " FIELD KEY: " . $field_key ;
+						update_field( "$field_key", $wl_api_main_address, $postInsertId);
+
 						// This is a little trick to "catch" the image id
 						// Attach/upload the "sideloaded" image
 						// And remove the little trick
