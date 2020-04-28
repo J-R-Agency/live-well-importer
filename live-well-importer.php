9fde80f1493ae40bd19878a7e01840f8b7ff31b3
Greg Macoy <greg.macoy@jnragency.co.uk>
2020-04-28 09:38:00 +0100

    Check for post title

--- live-well-importer.php
+++ live-well-importer.php
@@ -223,11 +223,11 @@ function live_well_importer_handle_post(){
 
 						if ( get_page_by_title( $postCreated["post_title"], OBJECT, "activities" ) == null ) {
 
-							echo "<br> NO POST TITLE FOUND IN DB";
+							echo "<br> NO POST TITLE FOUND IN DB: DOES NOT ALREADY EXIST";
 
 						} else {
 
-							echo "<br> POST TITLE: " . $postCreated["post_title"];
+							echo "<br> POST TITLE: " . $postCreated["post_title"] . " DOES ALREADY EXIST" ;
 						}
 
 
