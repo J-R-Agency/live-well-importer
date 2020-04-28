8ecae9041436ad0c0f085d6d922773e66a70d045
Greg Macoy <greg.macoy@jnragency.co.uk>
2020-04-28 10:21:49 +0100

    Check for post title

--- live-well-importer.php
+++ live-well-importer.php
@@ -75,7 +75,7 @@ function live_well_importer_handle_post(){
 			foreach ($api_data as $api_url) {
 				# code...
 
-				echo "<h1>" . $api_url . "</h1>" ;
+				echo "<h2>" . $api_url . "</h2>" ;
 
 			    $json_data = file_get_contents($api_url);  
 			    //convert json object to php associative array
